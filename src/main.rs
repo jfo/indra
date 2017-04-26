1302b27c4aa9a589bfd6036afa3deb025e46e72f
Jeff Fowler <jeffowler@gmail.com>
2017-04-26 01:08:51 -0400

    sort of

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,52 @@ use nom::*;
 type Link = Option<Box<Cell>>;
 
 #[derive(Debug)]
-enum Car {
+enum Val {
     Token(&'static[u8]),
-    // Cell,
+    List(Link),
 }
 
 #[derive(Debug)]
 struct Cell {
-    car: Car,
+    car: Val,
     cdr: Link,
 }
 
-named!(token, ws!(alpha));
+enum MatchReturn {
+    Token(&'static[u8]),
+    List(&'static[u8]),
+}
+
+named!(
+    token(&'static[u8]) -> MatchReturn,
+    do_parse!(
+        x: ws!(alpha) >>
+        (MatchReturn::Token(x))
+    )
+);
+named!(
+    list(&'static[u8]) -> MatchReturn,
+    do_parse!(
+        x: ws!(delimited!(tag!("("), take_until!(")"), tag!(")"))) >>
+        (MatchReturn::List(x))
+    )
+);
+named!(
+    cell(&'static[u8]) -> MatchReturn,
+    alt!(list | token)
+);
 
 fn make_cell(input: &'static[u8]) -> Cell {
-    match token(input) {
+    match cell(input) {
         IResult::Done(rest, matched) => Cell {
-            car: Car::Token(matched),
-            cdr: match token(rest) {
+            car: match matched {
+                MatchReturn::Token(x) => Val::Token(x),
+                MatchReturn::List(x) => Val::List(Some(Box::new(make_cell(x))))
+            },
+            cdr: match cell(rest) {
                 IResult::Done(_, _) => Some(Box::new(make_cell(rest))),
                 IResult::Incomplete(_) => None,
-                IResult::Error(_) => panic!(),
+                IResult::Error(_) => None
             }
         },
         _ => panic!(),
@@ -33,6 +58,6 @@ fn make_cell(input: &'static[u8]) -> Cell {
 }
 
 fn main(){
-    let string = b"holy shit this worked";
+    let string = b"(holy) crap it (works)";
     println!("{:?}", make_cell(string));
 }
